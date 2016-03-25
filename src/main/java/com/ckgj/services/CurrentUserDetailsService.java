8c5167a0cef331cc6e4f82482f65cf11284a72b9
Calvin Z <calvin.zkl@qq.com>
2016-03-25 23:17:35 +0800

    t

---
+++ src/main/java/com/ckgj/services/CurrentUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.ckgj.services;
+
+import com.ckgj.models.CurrentUser;
+import com.ckgj.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by zhaok_000 on 3/25/2016.
+ */
+
+@Service
+public class CurrentUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    @Autowired
+    public CurrentUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public CurrentUser loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.getUserByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("User with email=%s was not found", email)));
+        return new CurrentUser(user);
+    }
+}
