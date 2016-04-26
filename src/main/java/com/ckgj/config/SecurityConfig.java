dc864a6de1baa25268f0c2e796be84bb5b1eba4a
Calvin Z <calvin.zkl@qq.com>
2016-04-26 11:33:11 +0800

    release

--- src/main/java/com/ckgj/config/SecurityConfig.java
+++ src/main/java/com/ckgj/config/SecurityConfig.java
@@ -7,11 +7,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-//@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 @EnableWebSecurity
 class SecurityConfig extends WebSecurityConfigurerAdapter {
-    //@Autowired
-    //private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -21,25 +18,10 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and()
                 .httpBasic();
-//        http.authorizeRequests()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .failureUrl("/login?error")
-//                .usernameParameter("phone")
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .logoutUrl("/logout")
-//                .logoutSuccessUrl("/")
-//                .permitAll();
     }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .userDetailsService(userDetailsService)
-//                .passwordEncoder(new BCryptPasswordEncoder());
         auth
                 .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
